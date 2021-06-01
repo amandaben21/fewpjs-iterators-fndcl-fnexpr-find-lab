27d32ae5fe127e4a00f926ed752e9eabdd09d4ca
Amanda <amandaben21@aol.com>
2021-06-01 14:21:09 -0400

    commit tree 17d8b1f50a18b23d962a46a8305e6e9b2523156e

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(array) {
+    let winner = array.find(win => {
+        return win.result === "W"
+    })
+    if (winner === undefined) {
+        return winner
+    } else {
+        return winner.year
+    }
+}
